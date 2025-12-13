d36588fb797f5c7ead2867eb0b27e73d3aa8c10f
MGCODEdev <markus@gmeinhart.net>
2025-12-13 15:23:55 +0100

    Update

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ $is_member = is_logged_in(); // True for Club Admin & Super Admin
     <title>MotoCalendar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet">
-    <link href="assets/css/style.css?v=5" rel="stylesheet">
+    <link href="assets/css/style.css?v=6" rel="stylesheet">
 </head>
 
 <body>
